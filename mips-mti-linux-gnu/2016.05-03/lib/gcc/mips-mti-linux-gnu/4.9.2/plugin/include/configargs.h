44ed81f7d58e45d94c99b8de7d0420f352f3f163
Gaurav Pathak <>
2020-08-25 10:34:18 +0530

    Adding toolchain with  configuration for MIPS Target

---
+++ mips-mti-linux-gnu/2016.05-03/lib/gcc/mips-mti-linux-gnu/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/scratch/mpf/jobs/54397/48138/240198/shared/gcc/configure --prefix=/scratch/mpf/jobs/54397/48138/240258/shared/mips-mti-linux-gnu/2016.05-03 --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --disable-libssp --disable-libmudflap --disable-decimal-float --with-mips-plt --target=mips-mti-linux-gnu --enable-languages=c,c++,fortran --with-sysroot=/scratch/mpf/jobs/54397/48138/240258/shared/mips-mti-linux-gnu/2016.05-03/sysroot --enable-__cxa_atexit --with-bugurl=http://imgtec.com/mips-sdk-support/ --with-pkgversion='Codescape GNU Tools 2016.05-03 for MIPS MTI Linux'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch_32", "mips32r2" }, { "arch_64", "mips64r2" }, { "llsc", "llsc" }, { "mips-plt", "plt" } };
